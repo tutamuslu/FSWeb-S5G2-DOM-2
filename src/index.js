6081ecced0966cd2194f1f241675895672173d02
Tugba Muslu <tugba.evli05@gmail.com>
2023-05-24 21:45:53 +0300

    5.hafta 2.gün çalışma

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+document.querySelector("h1").addEventListener("click",function(e){
+alert("Eğlence otobüsündesin!!")
+});
 
+document.querySelector("header img").addEventListener("mouseover", function(e){
+    document.querySelector("header img").style.border = "5px solid #000"
+});
 
-// Kodlar buraya gelecek!
+document.querySelector("header img").addEventListener("mouseleave", function(e){
+    document.querySelector("header img").style.border = "0"
+});
+
+document.addEventListener("keypress", function(e) {
+    if(e.key == "a"){
+        e.preventDefault();
+        alert("a ya basıldı")
+    }
+})
+
+const images = document.querySelectorAll("img");
+
+images.forEach(img => {
+    img.addEventListener("dblclick", function(e){
+        img.style.width = '80%'
+    })
+});
+
+window.addEventListener("load", function(e){
+    setTimeout(() => {
+        this.document.querySelector("body").style.background = "lightgray"
+    }, (2000));
+})
+
+document.querySelectorAll("nav a").addEventListener("click", function(e){
+    e.preventDefault();
+    alert("link pasif edildi")
+})
+
+document.querySelector(".intro h2").addEventListener("mouseenter", function(e){
+    document.querySelector(".intro h2").style.color = "red"
+})
